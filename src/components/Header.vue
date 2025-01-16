1a9c2cf7abd242837fef52e1883b34eadf9312c6
Henrik Santos <henrik.ruan4@gmail.com>
2025-01-16 11:37:20 -0300

    update index.html and Header.vue for localization and branding; add brand image

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,14 +1,14 @@
-<script setup lang="ts">
-import { RouterView } from 'vue-router'
-</script>
-
 <template>
   <nav
-    class="flex justify-between p-3 bg-zinc-900 items-center align-middle lg:px-32 xl:px-[32rem]"
+    class="flex justify-between py-5 bg-zinc-900 items-center align-middle lg:px-32 xl:px-[32rem]"
   >
-    <RouterLink to="/" class="text-white text-3xl font-extrabold"
-      ><span class="text-green-500">G</span>ame<span class="text-green-500">S</span>helf</RouterLink
-    >
+    <div class="flex gap-2 items-center">
+      <img src="../assets/brand.png" alt="brand" class="w-12" />
+      <RouterLink to="/" class="text-white text-3xl font-extrabold"
+        ><span class="text-green-500">G</span>ame<span class="text-green-500">S</span
+        >helf</RouterLink
+      >
+    </div>
 
     <div class="flex gap-5 uppercase text-sm font-bold">
       <RouterLink to="/signin" class="hover:text-white">Sign In</RouterLink>
